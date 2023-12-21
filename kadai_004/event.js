b55362c16f9461e2def1e417dcccc55c4121b429
endo6614 <145337649+endo6614@users.noreply.github.com>
2023-12-21 13:41:15 +0900

    ファイルをアップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,23 @@
+// loadイベント
+$(window).on('load',()=>{
+  console.log('loadイベントが発生しました');
+});
+
+// $(window).on('load',function(){
+//   console.log('loadイベントが発生しました');
+// });
+
+// $(function(){
+//   console.log('loadイベントが発生しました')
+// });
+
+
+// scrollイベント
+$(window).on('scroll',()=>{
+  console.log('scrollイベントが発生しました');
+});
+
+// $(window).on('scroll',function(){
+//   console.log('scrollイベントが発生しました');
+// });
+
